ff05d775a5ea2c8a99fc218018bfaec20091e7a4
RiteshJha912 <141942049+RiteshJha912@users.noreply.github.com>
2024-07-29 02:12:55 +0530

    keys removed

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,26 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-import { getAuth,GoogleAuthProvider } from 'firebase/auth'
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAu-Fs_dt3HiFZUh7B3h-QWKobGvyNFfkE',
-  authDomain: 'expense-tracker99.firebaseapp.com',
-  projectId: 'expense-tracker99',
-  storageBucket: 'expense-tracker99.appspot.com',
-  messagingSenderId: '97635266436',
-  appId: '1:97635266436:web:a64fb484aee1b641bcb7d1',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
-export const db = getFirestore(app);
-
-// firebase login
-// firebase init
-// firebase deploy
+export const auth = getAuth(app)
+export const provider = new GoogleAuthProvider()
+export const db = getFirestore(app)
